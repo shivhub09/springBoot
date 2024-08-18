790aa616c92cb29e87b5a887e8b34d646b1768a8
shivhub09 <shivnagori2020@gmail.com>
2024-08-18 15:46:45 +0530

    initial springboot project

---
+++ src/test/java/com/exampledemo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampledemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
